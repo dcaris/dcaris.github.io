afe98d52c102350da2cc48efe81c8bd76ea97ea7
Daniel Caris <dcaris86@gmail.com>
2018-02-23 10:42:32 +1000

    flexboxgrid implemented. Need to enforce Col into layout and correct styling previosuly used

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Sidebar from '../components/Sidebar';
 
+import {Grid, Row, Col} from 'react-flexbox-grid';
+import {StyleSheet, css} from 'aphrodite';
+import * as colours from '../styles/colours';
+import * as viewports from '../styles/viewport';
+import '../styles/index.scss'
+
+const styles = StyleSheet.create({
+    dcBody: {
+        padding: '0px',
+        width: '100%',
+        height: '100%'
+    },
+    dcBodyRow: {
+        //display: 'table',
+        width: '100%',
+        height: '100%'
+    },
+    dcBodyContent: {
+        padding: '0px',
+        width: '100%',
+        height: '100%',
+        backgroundColor: colours.bgDefault,
+        marginLeft: '20px',
+        marginTop: '-10px',
+        [viewports.atLeastTablet]: {
+            //overflowY: 'scroll'
+            marginLeft: '40px',
+            marginTop: '-10px'
+        }
+    },
+    dcBodyContentParagraph: {
+        color: 'white',
+        margin: '10px',
+        textAlign: 'justify',
+        fontSize: '20px'
+    }
+});
+
 const TemplateWrapper = ({children}) => (
-    <div className="container dc-body">
+    <Grid className={css(styles.dcBody)}>
         <Helmet
             title="Daniel Caris Site"
             meta={[{
@@ -18,13 +55,17 @@ const TemplateWrapper = ({children}) => (
                 href: "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
             }
         ]}/>
-        <div className="row">
-            <Sidebar/>
-            <div>
-                {children()}
-            </div>
-        </div>
-    </div>
+        <Row className={css(styles.dcBodyRow)}>
+            <Col xs={12} sm={3} md={3} lg={3} xl={3}>
+                <Sidebar/>
+            </Col>
+            <Col xs={12} sm={9} md={9} lg={9} xl={9}>
+                <div className={css(styles.dcBodyContent)}>
+                    {children()}
+                </div>
+            </Col>
+        </Row>
+    </Grid>
 )
 
 TemplateWrapper.propTypes = {
--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,24 +1,92 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import Link from 'gatsby-link'
 
-import '../styles/post.scss';
+import {Grid, Row, Col} from 'react-flexbox-grid';
+import {StyleSheet, css} from 'aphrodite';
+import * as colours from '../styles/colours';
+import * as viewports from '../styles/viewport';
+// import '../styles/post.scss';
 
-export default function Template({
-  data
-}) {
-  const { markdownRemark: post } = data;
+const styles = StyleSheet.create({
+  dcPost: {
+    margin: '30px',
+    fontFamily: 'Georgia, Cambria, "Times New Roman", Times, serif',
+    fontSize: '18px',
+    [viewports.atLeastTablet]: {
+        margin: '50px',
+    },
+  },
+  dcPostTitle: {
+    fontFamily: 'Droid Serif, serif',
+    fontWeight: '800',
+    fontSize: '60px',
+    lineHeight: '1em',
+    textDecorationLine: 'none',
+    color: colours.fontDefault,
+  },
+  dcPostTitleLink: {
+    color: colours.fontDefault,
+    transition: '0.5s ease',
+    ':hover ': {
+      color: colours.fontHighlight,
+      textDecorationLine: 'none',
+    }
+  },
+  dcPostTitleIcon: {
+    fontSize: '30px',
+    verticalAlign: 'middle',
+  },
+  dcPostMeta: {
+    color: 'rgb(187, 183, 183)',
+  },
+  dcPostMetaSpan: {
+    textDecorationLine: 'none',
+    marginRight: '10px',
+  },
+  dcPostMetaSpanIcon: {
+    marginRight: '10px'
+  },
+  dcPostMetaSpanLink: {
+    color: 'rgb(187, 183, 183)',
+    transition: '0.5s ease',
+    ':hover': {
+        textDecorationLine: 'none',
+        color: 'rgb(131, 128, 128)',
+    }
+  },
+  dcPostContent: {
+      marginTop: '10px',
+  }
+});
+
+export default function Template({data}) {
+  const {markdownRemark: post} = data;
   return (
-    <div className="blog-post-container">
-      <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
-      <div className="blog-post">
-        <h1>{post.frontmatter.title}</h1>
-        <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: post.html }} />
+    <article className={css(styles.dcPost)}>
+      <Helmet title={`Daniel Caris - ${post.frontmatter.title}`}/>
+      <div className={css(styles.dcPost)}>
+        <h1 className={css(styles.dcPostTitle)}>{post.frontmatter.title}</h1>
+        <div className={css(dcPostMeta)}>
+            <span className="date"><i className="fa fa-calendar"></i>post.date</span>
+            <span className="category"><i className="fa fa-bookmark"></i>
+              category
+            </span>
+            <span class="tag"><i class="fa fa-tag"></i>
+                tags
+            </span>
+        </div>
+        <div
+          className={css(styles.dcPostContent)}
+          dangerouslySetInnerHTML={{
+          __html: post.html
+        }}/>
       </div>
-    </div>
+    </article>
   );
 }
 
-export const pageQuery = graphql`
+export const pageQuery = graphql `
   query BlogPostByPath($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
@@ -29,5 +97,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
-;
+`;
